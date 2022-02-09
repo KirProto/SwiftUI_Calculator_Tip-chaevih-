490d986f15667f1eb8965f383df8ec4cef9cd384
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 14:56:57 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Calculator_Tip(chaevih)/Yan_SwiftUI_Calculator_Tip_chaevih_App.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Calculator_Tip_chaevih_App.swift
+//  Yan_SwiftUI_Calculator_Tip(chaevih)
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Calculator_Tip_chaevih_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
